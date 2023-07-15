4c13b0a8ceed5a4ff3b1f4e812385b8546e0bb30
bmanss <blake_a_tucker@live.com>
2023-07-15 00:21:30 -0400

    Initial commit

---
+++ Catawba/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Catawba.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Catawba
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new ViewModelHandler()
+            };
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+    }
+}
